5ba70ca36f668344e1540861e35da1c080db8726
zhining-lu <zhining_lu@163.com>
2022-10-21 00:26:40 +0800

    Upgrade to version 1.3.1

--- bin/start-local-quic-proxy.sh
+++ bin/start-local-quic-proxy.sh
@@ -9,4 +9,4 @@ fi
 homepath=`pwd`
 log=$homepath/logs/sw-proxy.log
 echo "starting client proxy program, logging to $log"
-nohup java -Xms512m -Xmx512m -jar ./lib/netty-quic-proxy-1.3.0.jar -c -conf="./conf/config-example-client.json" >/dev/null 2>&1 &
+nohup java -Xms512m -Xmx512m -jar ./lib/netty-quic-proxy-1.3.1.jar -c -conf="./conf/config-example-client.json" >/dev/null 2>&1 &
